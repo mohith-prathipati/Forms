15ceb512be780c5720e2b6a87f5c34a313ec1759
mohith-prathipati <mprathip@gitam.in>
2026-05-11 14:37:11 +0530

    Completed CRUD and backend API

--- server/controllers/surveyControllers.js
+++ server/controllers/surveyControllers.js
@@ -22,6 +22,103 @@ const createSurvey = async (req, res) => {
   }
 };
 
+const getSurveys = async (req, res) => {
+  try {
+    const surveys = await Survey.find();
+
+    res.status(200).json(surveys);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const getSurveyById = async (req, res) => {
+  try {
+    const survey = await Survey.findById(
+      req.params.id
+    );
+
+    if (!survey) {
+      return res.status(404).json({
+        message: "Survey not found",
+      });
+    }
+
+    res.status(200).json(survey);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const updateSurvey = async (req, res) => {
+  try {
+    const { title, questions } = req.body;
+
+    const updatedSurvey =
+      await Survey.findByIdAndUpdate(
+        req.params.id,
+        {
+          title,
+          questions,
+        },
+        {
+          new: true,
+        }
+      );
+
+    if (!updatedSurvey) {
+      return res.status(404).json({
+        message: "Survey not found",
+      });
+    }
+
+    res.status(200).json(updatedSurvey);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const deleteSurvey = async (req, res) => {
+  try {
+    const deletedSurvey =
+      await Survey.findByIdAndDelete(
+        req.params.id
+      );
+
+    if (!deletedSurvey) {
+      return res.status(404).json({
+        message: "Survey not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Survey deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
 module.exports = {
   createSurvey,
+  getSurveys,
+  getSurveyById,
+  updateSurvey,
+  deleteSurvey,
 };
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -5,8 +5,20 @@ const router = express.Router();
 //Redirects to controller
 const {
   createSurvey,
+  getSurveys,
+  getSurveyById,
+  updateSurvey,
+  deleteSurvey,
 } = require("../controllers/surveyControllers");
 
 router.post("/", createSurvey);
+
+router.get("/", getSurveys);
+
+router.get("/:id",getSurveyById);
+
+router.put("/:id",updateSurvey);
+
+router.delete("/:id",deleteSurvey);
 
 module.exports = router;
